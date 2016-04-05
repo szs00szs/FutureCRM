a8f2480017e5daaa57a745b796e8d51cecb8f12e
wangshigen <979300544@qq.com>
2016-04-05 22:07:34 +0800

    sdfsd

--- src/com/future/hist/crm/dao/UserMapper.java
+++ src/com/future/hist/crm/dao/UserMapper.java
@@ -1,6 +1,23 @@
 package com.future.hist.crm.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.future.hist.crm.domain.Department;
+import com.future.hist.crm.domain.User;
 
 public interface UserMapper {
+
+	User getUserByName(String name);
+
+	void insert(User user);
+
+	void update(User user);
+
+	void deleteById(String user_id);
+
+	List<Department> getAllUserByPage(Map<String, Object> map);
+
+	User getById(String user_id);
 
 }
