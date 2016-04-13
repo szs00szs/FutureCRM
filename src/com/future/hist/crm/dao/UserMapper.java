a70bc8a598f47daf9bdd3ff9216e9e869818b04a
wangshigen <979300544@qq.com>
2016-04-13 09:10:17 +0800

    gfh

--- src/com/future/hist/crm/dao/UserMapper.java
+++ src/com/future/hist/crm/dao/UserMapper.java
@@ -6,6 +6,30 @@ import java.util.Map;
 import com.future.hist.crm.domain.User;
 
 public interface UserMapper {
-    public User getUserById(Long id);
+    int count();
+    
+    int deleteByUser(User user);
+
+    int deleteById(long id);
+
+    int addUser(User user);
+
+    int addSelective(User user);
+
+    List<User> selectByUser(User user);
+
+    User selectById(long id);
+
+	User getUserByName(String name);
+
+	void insert(User user);
+
+	void update(User user);
+
+	void deleteById(String user_id);
+
+	List<User> getAllUserByPage (Map<String, Object> map);
+
+	User getById(String user_id);
 
 }
